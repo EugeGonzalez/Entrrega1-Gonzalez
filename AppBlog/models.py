76ea17d6e43059311f3937582e4d10d8bc8d63d9
Euge Gonzalez <gonzalezm.euge97@gmail.com>
2022-07-12 10:07:57 -0300

    Inicio Proyecto

---
+++ AppBlog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+
+class Admin (models.Model):
+    nombre_usuario = models.CharField(max_length=50)
+    contraseña  = models.CharField(max_length= 30)
+    tipo_usuario = models.CharField(max_length= 20)
+
+    def __integer__(self):
+        return self.nombre_usuario
+
+
+class InicioSesion(models.Model):
+    nombre_usuario=models.CharField(max_length=50)
+    contraseña= models.CharField(max_length=30)
+
+
+
+
+class CrearUsuario(models.Model):
+    nombre=models.CharField(max_length=50)
+    nombre_usuario=models.CharField(max_length=50)
+    email=models.EmailField(max_length=60)
+    fecha_nacimiento=models.DateField()
+    contraseña= models.CharField(max_length=30)
