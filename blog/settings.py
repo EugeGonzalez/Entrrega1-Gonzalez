cc93eda924d0235694df767cf19b9bedc8451559
Euge Gonzalez <gonzalezm.euge97@gmail.com>
2022-07-12 13:54:34 -0300

    correcciones

--- blog/settings.py
+++ blog/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/eushe/OneDrive/Desktop/Entrega1+Python/blog/AppBlog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
