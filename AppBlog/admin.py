c85c49ce438cb71808b4cc994137b4cebe98644d
Euge Gonzalez <gonzalezm.euge97@gmail.com>
2022-07-13 00:23:15 -0300

    creacion de admin

--- AppBlog/admin.py
+++ AppBlog/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 
-# Register your models here.
+admin.site.register(Admin)
+admin.site.register(InicioSesion)
+admin.site.register(CrearUsuario)
+admin.site.register(Contacto)
